a52cc0d261eabec5feb77d404fadfe84a57dcebc
Kevin Zheng <jiezheng2020@gmail.com>
2021-06-18 18:45:09 -0500

    final details on about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,10 +10,11 @@ export default function AboutMe() {
         Mechanical Engineering. During my time there, I realized that my
         passions didn't align with what I was learning. I was always drawn to
         the design and problem solving aspect of MechE more so than the theory
-        behind it. I believe that the tech industry is the future and can impact
-        people's lives in ways the traditional manufacturing industry cannot,
-        and I hope to become someone that can do that through software and
-        technology.
+        behind it. After my first major work experience, I saw shortcomings in
+        how much impact I felt like my work was really producing. I believe that
+        the tech industry is the future and can impact people's lives in ways
+        the traditional manufacturing industry cannot, and I hope to become
+        someone that can do that through software and technology.
       </div>
     </div>
   );
