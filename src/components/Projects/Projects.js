f58a5682b4d2f54541bdcf97f5b6ddba3ae078f1
Kevin Zheng <jiezheng2020@gmail.com>
2021-06-19 21:49:45 -0500

    projects page started

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,17 @@
 import "./Projects.css";
 
 export default function Projects() {
-  return <div id="projects"></div>;
+  return (
+    <div id="projects">
+      <div className="d-flex flex-column align-items-center">
+        <h2 style={{ fontSize: "30px" }}>Projects</h2>
+        <div className="d-flex projects-row">
+          <div className="project-container">Project 1</div>
+          <div className="project-container">Project 1</div>
+          <div className="project-container">Project 1</div>
+          <div className="project-container">Project 1</div>
+        </div>
+      </div>
+    </div>
+  );
 }
