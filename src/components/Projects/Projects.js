d9b69b2ee60701fd456562f52431ea969b579917
Kevin Zheng <jiezheng2020@gmail.com>
2021-06-19 22:12:44 -0500

    project buttons added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,7 @@
 import "./Projects.css";
+import calorie from "./calorie.PNG";
+import harmony from "./harmony.PNG";
+import wanderlust from "./wanderlust.PNG";
 
 export default function Projects() {
   return (
@@ -6,10 +9,25 @@ export default function Projects() {
       <div className="d-flex flex-column align-items-center">
         <h2 style={{ fontSize: "30px" }}>Projects</h2>
         <div className="d-flex projects-row">
-          <div className="project-container">Project 1</div>
-          <div className="project-container">Project 1</div>
-          <div className="project-container">Project 1</div>
-          <div className="project-container">Project 1</div>
+          <div className="project-container">
+            <img className="proj-image" src={calorie} />
+            <div className="d-flex flex-row proj-links">
+              <a>Github Repo</a>
+              <a>Live Link</a>
+            </div>
+          </div>
+          <div className="project-container d-flex flex-column">
+            <img className="proj-image" src={harmony} />
+            Project 1
+          </div>
+          <div className="project-container">
+            <img className="proj-image" src={wanderlust} />
+            Project 1
+          </div>
+          <div className="project-container">
+            <div className="proj-image" />
+            Project 1
+          </div>
         </div>
       </div>
     </div>
