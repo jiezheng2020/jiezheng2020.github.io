886ab9a07bb4d23a218c2428af722009cbbcf534
Kevin Zheng <jiezheng2020@gmail.com>
2021-07-01 11:45:32 -0500

    commented out dead links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -26,7 +26,7 @@ export default function NavBar() {
           <Nav.Link href="#about">About Me</Nav.Link>
           <Nav.Link href="#projects">Projects</Nav.Link>
           <Nav.Link href="#skills">Skills</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          {/* <Nav.Link href="#contact">Contact</Nav.Link> */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
